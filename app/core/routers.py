0383d4dda51b4f1b5b8aa82eaa5c9028be335535
R-S-9 <rafik.saakyan.2016@mail.ru>
2023-01-01 16:03:05 +0700

    add feed url

--- app/core/routers.py
+++ app/core/routers.py
@@ -1,11 +1,28 @@
 from rest_framework.routers import DefaultRouter
 
-from apps.account.viewsets import TeacherViewSet
-from apps.feed.viewsets import CoursesViewSet, ReviewsViewSet
+from apps.account.viewsets import TeacherViewSet, UserViewSet
+from apps.feed.viewsets import (
+    CoursesViewSet,
+    ReviewsViewSet,
+    CourseProgramViewSet,
+    CourseCompositionViewSet,
+)
 
 
 router = DefaultRouter()
 
+# account
 router.register(r"teachers", TeacherViewSet, basename="teachers")
+router.register(r'users', UserViewSet, basename='users')
+
+# feed
 router.register(r"courses", CoursesViewSet, basename="courses")
 router.register(r"reviews", ReviewsViewSet, basename="Reviews")
+router.register(
+    "course_program", CourseProgramViewSet, basename="course_program"
+)
+router.register(
+    r"course_composition",
+    CourseCompositionViewSet,
+    basename="course_composition"
+)
